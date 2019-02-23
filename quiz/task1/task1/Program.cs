d2907b9ca7cd7ac5a9cd39b938298b87d13cfc95
Aidar <azhmetov.aidar@mail.ru>
2019-02-23 06:16:23 +0600

    arsen master

---
+++ quiz/task1/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir1 = new DirectoryInfo(@"C:\Users\Arsen Zhaxygaliyev\Desktop\Новая папка\");
+            FileSystemInfo[] a = dir1.GetFileSystemInfos();
+            foreach(FileSystemInfo files in a)
+            {
+                if (a.Length % 2 != 0) Console.WriteLine(files + " "); 
+            }
+        
+        }
+    }
+}
+
---
+++ quiz/training/training/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+        public static void Direc(DirectoryInfo directory, string s)
+        {
+            FileInfo[] files = directory.GetFiles();//сохраняем информацию о файлах в папке
+            DirectoryInfo[] directories = directory.GetDirectories();//сохраняем информацию о папках 
+                int cnt = 0;
+                foreach (DirectoryInfo dir in directories)
+                {
+                    
+                    Console.WriteLine(dir.Name + " " + cnt);
+                    Direc(dir, s);
+                
+
+                }
+            foreach (FileInfo file in files)
+                    {
+                        if (file.Name.StartsWith(s) || dir.Name.StartsWith(s))
+                        {
+                            cnt++;
+                        }
+                        Console.WriteLine(file.Name);
+                    }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Arsen Zhaxygaliyev\Desktop\Новая папка");//сохраняем информацию о папке
+            string s = Console.ReadLine();
+            Direc(directory, s);
+
+        }
+    }
+}
